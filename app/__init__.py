71dbbff0f0169673e6349d4497a448a4c9ff9e48
Jeter Chen <dali1756@gmail.com>
2025-02-13 23:03:15 +0800

    feat:會員系統

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
