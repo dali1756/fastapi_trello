086fa5bc0b96b1ee5530a1a2dcbdf29c810da2b1
Jeter Chen <dali1756@gmail.com>
2025-02-17 21:10:14 +0800

    feat:忘記執行alembic upgrade head

---
+++ app/projects/views.py
@@ -0,0 +1,10 @@
+# from fastapi import APIRouter, File, Depends, Form, HTTPException, Request, status
+# from app import templates
+# from models.projects import Project
+# from fastapi.responses import RedirectResponse
+# from sqlalchemy.orm import Session
+# from db.db import SessionLocal, engine
+# from passlib.context import CryptContext
+# from typing import Annotated
+
+# project = APIRouter()
---
+++ models/projects.py
@@ -0,0 +1,11 @@
+# from sqlalchemy import Column, Integer, Text, String, DateTime
+# from db.db import BaseModel
+
+# class Project(BaseModel):
+#     __tablename__ = "projects"
+    
+#     id = Column(Integer, primary_key=True, index=True)
+#     name = Column(String, nullable=False)
+#     description = Column(Text)
+#     start_date = Column(DateTime)
+#     end_date = Column(DateTime)
