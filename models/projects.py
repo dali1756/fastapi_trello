1f32879c9ee24f0f17159210f05938f6b9fd0724
Jeter Chen <dali1756@gmail.com>
2025-02-20 23:47:31 +0800

    feat:project CRUD

--- models/projects.py
+++ models/projects.py
@@ -1,11 +1,11 @@
-# from sqlalchemy import Column, Integer, Text, String, DateTime
-# from db.db import BaseModel
+from sqlalchemy import Column, Integer, Text, String, DateTime
+from db.db import BaseModel
 
-# class Project(BaseModel):
-#     __tablename__ = "projects"
+class Project(BaseModel):
+    __tablename__ = "projects"
     
-#     id = Column(Integer, primary_key=True, index=True)
-#     name = Column(String, nullable=False)
-#     description = Column(Text)
-#     start_date = Column(DateTime)
-#     end_date = Column(DateTime)
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    description = Column(Text)
+    start_date = Column(DateTime)
+    end_date = Column(DateTime)
