a99ad2989c9be3bf430b838b0f1a4170194b25ec
Jeter Chen <dali1756@gmail.com>
2025-05-12 21:12:18 +0800

    feat:使用者只能查看自己建立的專案

--- app/projects/views.py
+++ app/projects/views.py
@@ -5,6 +5,7 @@ from utils.get_db import get_db
 from models.projects import Project
 from models.lanes import Lane
 from models.users import User
+from models.user_projects import UserProject
 from typing import Annotated, Optional
 from schemas.project import ProjectCreate, ProjectUpdate
 from utils.auth import get_current_active_user
@@ -14,7 +15,7 @@ project = APIRouter()
 
 @project.get("/")
 async def index(request: Request, current_user: User = Depends(get_current_active_user), db: Session = Depends(get_db)):
-    projects = db.query(Project).order_by(Project.name.desc()).all()
+    projects = db.query(Project).join(UserProject, UserProject.project_id == Project.id).filter(UserProject.user_id == current_user.id).order_by(Project.name.desc()).all()
     is_htmx = request.headers.get("HX-Request") == "true"
     if is_htmx:
         content = templates.get_template("projects/partials/projects_list.html").render({"request": request, "projects": projects, "current_user": current_user})
@@ -32,9 +33,12 @@ async def create(request: Request, name: Annotated[str, Form()], current_user: U
     db.add(new_projects)
     db.commit()
     db.refresh(new_projects)
+    user_project = UserProject(user_id = current_user.id, project_id = new_projects.id)
+    db.add(user_project)
+    db.commit()
     is_htmx = request.headers.get("HX-Request") == "true"
     if is_htmx:
-        projects = db.query(Project).order_by(Project.name.desc()).all()
+        projects = db.query(Project).join(UserProject, UserProject.project_id == Project.id).filter(UserProject.user_id == current_user.id).order_by(Project.name.desc()).all()
         content = templates.get_template("projects/partials/projects_list.html").render({"projects": projects, "request": request})
         return HTMLResponse(content=content)
     else:
@@ -52,60 +56,61 @@ async def new(request: Request, current_user: User = Depends(get_current_active_
 @project.post("/{project_name}/update")
 async def update(project_name: str, name: Annotated[str, Form()], request: Request, description: Annotated[Optional[str], Form()] = None, current_user: User = Depends(get_current_active_user), db: Session = Depends(get_db)):
     update_data = ProjectUpdate(name = name, description = description)
-    projects = db.query(Project).filter(Project.name == project_name).first()
-    if not projects:
+    project_to_update = db.query(Project).join(UserProject, UserProject.project_id == Project.id).filter(Project.name == project_name, UserProject.user_id == current_user.id).first()
+    if not project_to_update:
         raise HTTPException(status_code=404, detail="查無專案。")
-    existing_project = db.query(Project).filter(Project.name == update_data.name, Project.id != projects.id).first()
+    existing_project = db.query(Project).filter(Project.name == update_data.name, Project.id != project_to_update.id).first()
     if existing_project:
         if request.headers.get("HX-Request") == "true":
             return HTMLResponse(content=f"""<div id="error-message" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">該專案名稱已被使用</div>""",status_code=400)
         else:
             raise HTTPException(status_code=400, detail="該專案名稱已被使用。")
-    projects.name = update_data.name
+    project_to_update.name = update_data.name
     if description is not None:
-        projects.description = update_data.description
+        project_to_update.description = update_data.description
     db.commit()
     is_htmx = request.headers.get("HX-Request") == "true"
     if is_htmx:
-        content = templates.get_template("projects/partials/projects_show.html").render({"request": request, "projects": projects, "current_user": current_user})
+        content = templates.get_template("projects/partials/projects_show.html").render({"request": request, "projects": project_to_update, "current_user": current_user})
         return HTMLResponse(content=content)
     else:
         return RedirectResponse(url="/projects", status_code=status.HTTP_302_FOUND)
 
 @project.get("/{project_name}")
 async def show(request: Request, project_name: str, current_user: User = Depends(get_current_active_user), db: Session = Depends(get_db)):
-    projects = db.query(Project).filter(Project.name == project_name).first()
-    if not projects:
+    project = db.query(Project).join(UserProject, UserProject.project_id == Project.id).filter(Project.name == project_name, UserProject.user_id == current_user.id).first()
+    if not project:
         raise HTTPException(status_code=404, detail="查無專案。")
-    lanes = db.query(Lane).filter(Lane.project_id == projects.id).all()
+    lanes = db.query(Lane).filter(Lane.project_id == project.id).all()
     is_htmx = request.headers.get("HX-Request") == "true"
     if is_htmx:
-        content = templates.get_template("projects/partials/projects_show.html").render({"request": request, "projects": projects, "lanes": lanes, "current_user": current_user})
+        content = templates.get_template("projects/partials/projects_show.html").render({"request": request, "projects": project, "lanes": lanes, "current_user": current_user})
         return HTMLResponse(content=content)
     else:
-        return templates.TemplateResponse("projects/show.html", {"request": request, "projects": projects, "lanes": lanes, "current_user": current_user})
+        return templates.TemplateResponse("projects/show.html", {"request": request, "projects": project, "lanes": lanes, "current_user": current_user})
 
 @project.get("/{project_name}/edit")
 async def edit(request: Request, project_name: str, current_user: User = Depends(get_current_active_user), db: Session = Depends(get_db)):
-    projects = db.query(Project).filter(Project.name == project_name).first()
-    if not projects:
+    project = db.query(Project).join(UserProject, UserProject.project_id == Project.id).filter(Project.name == project_name, UserProject.user_id == current_user.id).first()
+    if not project:
         raise HTTPException(status_code=404, detail="查無專案名稱。")
     is_htmx = request.headers.get("HX-Request") == "true"
     if is_htmx:
-        content = templates.get_template("projects/partials/projects_edit.html").render({"request": request, "projects": projects, "current_user": current_user})
+        content = templates.get_template("projects/partials/projects_edit.html").render({"request": request, "projects": project, "current_user": current_user})
         return HTMLResponse(content=content)
     else:
-        return templates.TemplateResponse("projects/edit.html", {"request": request, "projects": projects, "current_user": current_user})
+        return templates.TemplateResponse("projects/edit.html", {"request": request, "projects": project, "current_user": current_user})
 
 @project.post("/{project_name}/delete")
 async def delete(project_name: str, request: Request, current_user: User = Depends(get_current_active_user), db: Session = Depends(get_db)):
-    projects = db.query(Project).filter(Project.name == project_name).first()
-    if projects:
-        db.delete(projects)
+    project_to_delete = db.query(Project).join(UserProject, UserProject.project_id == Project.id).filter(Project.name == project_name, UserProject.user_id == current_user.id).first()
+    if project_to_delete:
+        db.query(UserProject).filter(UserProject.project_id == project_to_delete.id).delete()
+        db.delete(project_to_delete)
         db.commit()
         is_htmx = request.headers.get("HX-Request") == "true"
         if is_htmx:
-            projects = db.query(Project).order_by(Project.name.desc()).all()
+            projects = db.query(Project).join(UserProject, UserProject.project_id == Project.id).filter(UserProject.user_id == current_user.id).order_by(Project.name.desc()).all()
             content = templates.get_template("projects/partials/projects_list.html").render({"projects": projects, "request": request})
             return HTMLResponse(content=content)
         else:
