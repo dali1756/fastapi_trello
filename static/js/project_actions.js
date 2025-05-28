e018b4a39f762715e97bf2369404c0e23a362087
Jeter Chen <dali1756@gmail.com>
2025-05-28 20:30:35 +0800

    feat:多人協作

--- static/js/project_actions.js
+++ static/js/project_actions.js
@@ -8,3 +8,23 @@ function confirmDeleteProject (projectName) {
         }
     );
 }
+
+// 刪除成員
+function confirmRemoveMember(memberName, memberId, projectName) {
+    showConfirmAlert(
+        "刪除成員",
+        `確定要將 ${memberName} 從專案中刪除嗎？`,
+        function() {
+            htmx.ajax("POST", `/projects/${projectName}/remove_member`, {
+                values: { member_id: memberId },
+                target: "#members-content",
+                swap: "outerHTML"
+            }).then(() => {
+                showSuccessAlert(`已成功刪除 ${memberName}`);
+            }).catch((error) => {
+                showErrorAlert("刪除成員失敗");
+                console.error("刪除成員錯誤：", error);
+            });
+        }
+    );
+}
